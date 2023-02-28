7f187acdde1c11edbc997eb831aed5dd000afe48
Michael Laymon <miclaymon@outlook.com>
2023-02-28 10:19:36 -0600

    Implement suggested discard variable Add try-catch block for database creation

--- AbetApi/Program.cs
+++ AbetApi/Program.cs
@@ -19,7 +19,12 @@ namespace AbetApi
                 // This creates a database (described by the ABETDBContext class), if it doesn't already exist
                 // Changes to the ABETDBContext class will not apply changes to the existing database.
                 // If you don't have real data in the database, pick up changes via dropping your database, and run the program again. It will auto generate.
-                ctx.Database.EnsureCreated();
+                try {
+                    _ = ctx.Database.EnsureCreated();
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Failed to create Database: " + e.ToString());
+                }
                 //AbetApi.Data.Database.WipeTables();
             }
             CreateHostBuilder(args).Build().Run();
