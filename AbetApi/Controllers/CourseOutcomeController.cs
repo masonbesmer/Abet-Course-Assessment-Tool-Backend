90d010b37d84a040ff629fe7ebcfdeff3d30bbaf
Jakob Edgeworth <jakobedgeworth@gmail.com>
2023-11-14 11:57:12 -0600

    empty course outcomes list returns 200 OK

--- AbetApi/Controllers/CourseOutcomeController.cs
+++ AbetApi/Controllers/CourseOutcomeController.cs
@@ -69,6 +69,10 @@ namespace AbetApi.Controllers
             }
             catch (Exception ex)
             {
+                if(ex.Message == "The course specified has no course outcomes.")
+                {
+                    return Ok(ex.Message);
+                }
                 return BadRequest(ex.Message);
             }
         }
